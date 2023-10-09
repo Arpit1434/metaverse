53a29d1257b0b95cfe45ca912a931e758eaff8b2
Arpit1434 <arpit.tiwari1434@gmail.com>
2023-10-09 23:43:41 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const path = require("path")
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/public/views"));
+
+dotenv.config()
+const PORT = process.env.PORT
+
+app.listen(PORT)
+
+app.get("/", (req, res)=>{
+    res.render("index")
+})
+
+app.use((req, res)=>{
+    res.redirect("/")
+})
